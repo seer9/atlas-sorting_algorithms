be8201701bcbe0ebeec0ee2ed43f9cbaf0d5dd4a
Clay <rpctropic13@gmail.com>
2024-08-05 19:50:06 +0000

    typo

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,5 @@
 #include "sort.h"
 
-/**
- * swap_nodes - Swaps the values of two nodes
- * @head: Pointer to the head of the list
- * @first_node: Pointer to the first node to be swapped
- * @second_node: Pointer to the second node to be swapped
- */
 void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
 {
   if (first_node->prev)
@@ -19,13 +13,10 @@ void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
   first_node->next = second_node->next;
   second_node->prev = first_node->prev;
 
-  firs_node->prev = second_node;
+  first_node->prev = second_node;
   second_node->next = first_node;
 }
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * @list: Pointer to the head of the list
- */
+
 void insertion_sort_list(listint_t **list)
 {
   listint_t *current, *next;
