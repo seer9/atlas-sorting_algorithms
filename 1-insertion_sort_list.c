f4a54353110179c8abdc4ec2926edf9c024a132b
Clay <rpctropic13@gmail.com>
2024-08-05 21:30:30 +0000

    documentation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * swap_nodes - Swaps the values of two nodes
+ * 
+ * @head: head of the list
+ * @first_node: first node to be swapped
+ * @second_node: second node to be swapped
+ */
 void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node) 
 {
     if (first_node->prev) 
@@ -21,7 +28,11 @@ void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
     first_node->prev = second_node;
     second_node->next = first_node;
 }
-
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers 
+ *
+ * @list: head of the list
+ */
 void insertion_sort_list(listint_t **list) 
 {
     listint_t *current, *next, *temp;
