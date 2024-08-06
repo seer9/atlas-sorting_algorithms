f55cace8e9400f7f0ddc8176fe2b9449526f255f
root <rpctropic13@gmail.com>
2024-08-06 01:33:31 +0000

    betty checks

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,11 +8,11 @@
  */
 void simple_swp(int *first, int *second)
 {
-    int temp;
+	int temp;
 
-    temp = *first;
-    *first = *second;
-    *second = temp;
+	temp = *first;
+	*first = *second;
+	*second = temp;
 }
 /**
  * lomuto_part - simply pivots the array
@@ -25,29 +25,29 @@ void simple_swp(int *first, int *second)
  */
 int lomuto_part(int *array, int low, int high, size_t size)
 {
-    int pivot, i, j;
-    
-    pivot = array[high];
-    i = low;
-    
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            if (i != j)
-            {
-                simple_swp(&array[i], &array[j]);
-                print_array(array, size);
-            }
-            i++;
-        }
-    }
-    if (i != high)
-    {
-    simple_swp(&array[i], &array[high]);
-    print_array(array, size);
-    }
-    return (i);
+	int pivot, i, j;
+
+	pivot = array[high];
+	i = low;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			if (i != j)
+			{
+				simple_swp(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (i != high)
+	{
+		simple_swp(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
 }
 /**
  * sort_quik - Sorts an array of integers with quick sort
@@ -59,12 +59,14 @@ int lomuto_part(int *array, int low, int high, size_t size)
  */
 void sort_quik(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot_index = lomuto_part(array, low, high, size);
-        sort_quik(array, low, pivot_index - 1, size);
-        sort_quik(array, pivot_index + 1, high, size);
-    }
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_part(array, low, high, size);
+		sort_quik(array, low, pivot_index - 1, size);
+		sort_quik(array, pivot_index + 1, high, size);
+	}
 }
 /**
  * quick_sort - Sorts an array of integers with sort_quik
@@ -74,7 +76,7 @@ void sort_quik(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    sort_quik(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	sort_quik(array, 0, size - 1, size);
 }
