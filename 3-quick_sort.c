24cadc06fa448a51467cb8cfb4c3df81dc56aea3
Clay <rpctropic13@gmail.com>
2024-08-06 00:55:10 +0000

    simplified approach

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,30 @@
 #include "sort.h"
 
+/**
+ * simple_swp - Swaps two nodes
+ *
+ * @first: first node to be swapped
+ * @second: second node to be swapped
+ */
+void simple_swp(int *first, int *second)
+{
+    int temp;
+
+    temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+
 int lomuto_part(int *array, int low, int high, size_t size)
 {
+    int pivot, i, j;
+    
     int pivot = array[high];
-    int i = low - 1;
-    int j, temp;
+    int i = low;
+    int j;
+
+    
     for (j = low; j < high; j++)
     {
         if (array[j] <= pivot)
@@ -12,22 +32,19 @@ int lomuto_part(int *array, int low, int high, size_t size)
             i++;
             if (i != j)
             {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                simple_swp(&array[i], &array[j]);
                 print_array(array, size);
             }
         }
     }
     if (array[high] < array[i + 1])
     {
-        temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
+        simple_swp(&array[i + 1], &array[high]);
         print_array(array, size);
     }
     return i + 1;
 }
+
 
 void sort_helper(int *array, int low, int high, size_t size)
 {
@@ -38,6 +55,8 @@ void sort_helper(int *array, int low, int high, size_t size)
         sort_helper(array, pivot_index + 1, high, size);
     }
 }
+
+
 void quick_sort(int *array, size_t size)
 {
   int low, high, pivot;
